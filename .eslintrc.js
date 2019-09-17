11c2f5f9b6f4bab0cbb11748c900e7cb4252ecf6
ywenbin <ywenbin@localhost.com>
2019-09-17 23:42:09 +0800

    引入@umijs/fabric的lint配置，并且扩展自定义的eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,16 @@
+const { strictEslint } = require('@umijs/fabric');
+
 module.exports = {
-    "extends": ["airbnb"],
-    "root": true,
-    "env": {
-        "browser": true,
-        "es6": true
+    ...strictEslint,
+    rules: {
+        "indent": [2, 4],
+        "react/jsx-props-no-spreading": 0,
+        "react/jsx-indent": [2, 4],
+        "react/jsx-indent-props": [2, 4],
+        "comma-dangle": [2, "never"],
+        "no-console": 0
     },
-    "rules": {
-        "indent": ["error", 4],
-        "linebreak-style": "off",
-        "import/no-unresolved": ["error", {
-            "ignore": ["^@/"]
-        }],
-        "import/no-extraneous-dependencies": ["error", {
-            "devDependencies": ["**/tests/**.js", "/mock/**/**.js", "**/**.test.js"]
-        }],
-        "react/prop-types": ["off"],
-        "react/forbid-prop-types": ["off"],
-        "react/jsx-props-no-spreading": ["off"],
-        "react/jsx-filename-extension": ["error", {
-            "extensions": [".js", ".jsx"]
-        }],
-        "react/jsx-indent": ["error", 4],
-        "react/jsx-indent-props": ["error", 4],
-        "react/jsx-one-expression-per-line": ["off", {
-            "allow": "literal"
-        }],
-        // "react/no-unescaped-entities": ["off"],
-        "jsx-a11y/anchor-is-valid": "off",
-        "comma-dangle": ["error", "never"],
-        "no-console": "off"
-    },
-    "globals": {
+    globals: {
         "it": "readonly"
     }
 };
